76c6884614884d412f20a062bdbdd5f75568667b
Xiaxi Shen <shenxiaxi26@gmail.com>
2024-10-06 00:10:08 -0700

    update

--- Tree/Binary-Search-Tree/python/BST.py
+++ Tree/Binary-Search-Tree/python/BST.py
@@ -58,3 +58,62 @@ class BST:
       
     helper(self.root)
     return res
+
+  def findMax(self):
+    '''
+      - if node.right is None, return node.val
+      - else
+        - helper(node.right)
+    '''
+    def helper(node):
+      if not node.right:
+        return node.num
+      else:
+        return helper(node.right)
+    return helper(self.root)
+
+  '''
+  2
+    5
+  4   7
+    6
+
+  fP(5) -> 4
+  fp(7) -> 6
+  fp(4) -> 2
+
+  this node doesn't have left child, the prev is its parent
+  otherwise, the prev is node.left
+
+  Solution 1:
+    - do in order traversal, store the result
+    - find the prev element in the result array
+    - Time: O(N)
+    - Space: O(N)
+
+  Solution 2:
+    - record each node's parent using a map (need traverse the tree too)
+    - findPrev based on these 2 cases
+    - Time: O(N)
+    - Space: O(N)
+
+  Solution 3:
+    - do this on the fly
+    - Time: O(N) worst case
+
+  '''
+  def findPrev(self, num):
+    stack = []
+    cur = self.root
+    prev = None
+    res = [-1]
+    while cur or stack:
+      while cur:
+        stack.append(cur)
+        cur = cur.left
+      cur = stack.pop()
+      if cur.num == num:
+        return res[-1]
+      res.append(cur.num)
+      cur = cur.right
+  
--- Tree/Binary-Search-Tree/python/TestBST.py
+++ Tree/Binary-Search-Tree/python/TestBST.py
@@ -19,8 +19,6 @@ class TestBST(unittest.TestCase):
       self.assertFalse(self.bst.search(6))
       self.bst.insert(6)
       self.assertTrue(self.bst.search(6))
-
-
        
     def test_preorderTraversal(self):
         self.bst.insert(5)
@@ -31,6 +29,28 @@ class TestBST(unittest.TestCase):
         self.bst.insert(6)
         self.bst.insert(8)
         self.assertEqual(self.bst.preorderTraversal(), [5, 3, 2, 4, 7, 6, 8])
+
+    def test_findMax_happy_path(self):
+        self.bst.insert(5)
+        self.bst.insert(3)
+        self.bst.insert(7)
+        self.bst.insert(2)
+        self.bst.insert(4)
+        self.bst.insert(6)
+        self.bst.insert(8)
+        self.assertEqual(self.bst.findMax(), 8)      
+
+    def test_findPrev(self):
+        self.bst.insert(5)
+        self.bst.insert(3)
+        self.bst.insert(7)
+        self.bst.insert(2)
+        self.bst.insert(4)
+        self.bst.insert(6)
+        self.bst.insert(8)
+        self.assertEqual(self.bst.findPrev(8), 7)  
+        self.assertEqual(self.bst.findPrev(7), 6)  
+        self.assertEqual(self.bst.findPrev(2), -1)  
 
 if __name__ == '__main__':
     unittest.main()
