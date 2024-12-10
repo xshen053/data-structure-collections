4456da0eb134a82f8e9abe98e4e415330728330a
Xiaxi Shen <shenxiaxi26@gmail.com>
2024-12-10 11:39:02 -0800

    Upload

---
+++ Algorithms/sorting/quick-select/Readme.md
@@ -0,0 +1,5 @@
+# 快速选择
+
+- 215. Kth Largest Element in an Array
+- 347. Top K Frequent Elements
+- 1985. Find the Kth Largest Integer in the Array （和 215 一样）
--- Algorithms/sorting/quick-sort/QuickSort.py
+++ Algorithms/sorting/quick-sort/QuickSort.py
@@ -1,3 +1,26 @@
+import random
+
+# 这个是更简单直观的方法
+class Solution:
+    def sortArray(self, nums):
+        def quick_sort(nums):
+            if len(nums) < 2:
+                return nums
+            pivot = random.choice(nums)
+            left, mid, right = [], [], []
+            # partition
+            for num in nums:
+                if num < pivot:
+                    left.append(num)
+                elif num > pivot:
+                    right.append(num)
+                else:
+                    mid.append(num)    
+            return quick_sort(left) + mid + quick_sort(right)
+        return quick_sort(nums)
+
+
+# 这个方法partition太容易错了
 def sortArray(nums: list[int]) -> list[int]:
     """
     Quick Sort:
